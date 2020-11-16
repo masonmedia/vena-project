dbd9a338d180937175b882c34251a7ad11f2473b
Andrew Mason <andrewmasonmedia@gmail.com>
2020-11-16 10:38:54 -0500

    Added AOS animation, reconfigured main.js with self invoking functions

--- js/main.js
+++ js/main.js
@@ -1,63 +1,58 @@
-// nav toggle
-// function navToggle() {
-//     var btn = document.getElementById('menuBtn');
-//     var nav = document.getElementById('menu');
-
-//     btn.classList.toggle('open');
-//     nav.classList.toggle('flex');
-//     nav.classList.toggle('hidden');
-// }
-
+(function () {
+    AOS.init();
+  }());
+  
 // navToggle();
 
-const btn = document.getElementById('menuBtn');
-const nav = document.getElementById('menu');
+(function () {
+    const btn = document.getElementById('menuBtn');
+    const nav = document.getElementById('menu');
 
-menuBtn.addEventListener('click', function() {
+    btn.addEventListener('click', function() {
     // nav.classList.toggle('show');
     nav.classList.toggle('menu');
-})
+    })
+  }());
 
 // modal
-var openmodal = document.querySelectorAll('.modal-open')
-    for (var i = 0; i < openmodal.length; i++) {
-      openmodal[i].addEventListener('click', function(event){
-    	event.preventDefault()
-    	toggleModal()
-      })
-    }
+// var openmodal = document.querySelectorAll('.modal-open')
+//     for (var i = 0; i < openmodal.length; i++) {
+//       openmodal[i].addEventListener('click', function(event){
+//     	event.preventDefault()
+//     	toggleModal()
+//       })
+//     }
     
-    const overlay = document.querySelector('.modal-overlay')
-    overlay.addEventListener('click', toggleModal)
+//     const overlay = document.querySelector('.modal-overlay')
+//     overlay.addEventListener('click', toggleModal)
     
-    var closemodal = document.querySelectorAll('.modal-close')
-    for (var i = 0; i < closemodal.length; i++) {
-      closemodal[i].addEventListener('click', toggleModal)
-    }
+//     var closemodal = document.querySelectorAll('.modal-close')
+//     for (var i = 0; i < closemodal.length; i++) {
+//       closemodal[i].addEventListener('click', toggleModal)
+//     }
     
-    document.onkeydown = function(evt) {
-      evt = evt || window.event
-      var isEscape = false
-      if ("key" in evt) {
-    	isEscape = (evt.key === "Escape" || evt.key === "Esc")
-      } else {
-    	isEscape = (evt.keyCode === 27)
-      }
-      if (isEscape && document.body.classList.contains('modal-active')) {
-    	toggleModal()
-      }
-    };
+//     document.onkeydown = function(evt) {
+//       evt = evt || window.event
+//       var isEscape = false
+//       if ("key" in evt) {
+//     	isEscape = (evt.key === "Escape" || evt.key === "Esc")
+//       } else {
+//     	isEscape = (evt.keyCode === 27)
+//       }
+//       if (isEscape && document.body.classList.contains('modal-active')) {
+//     	toggleModal()
+//       }
+//     };
     
-    function toggleModal () {
-      const body = document.querySelector('body')
-      const modal = document.querySelector('.modal')
-      modal.classList.toggle('opacity-0')
-      modal.classList.toggle('pointer-events-none')
-      body.classList.toggle('modal-active')
-    }
+//     function toggleModal () {
+//       const body = document.querySelector('body')
+//       const modal = document.querySelector('.modal')
+//       modal.classList.toggle('opacity-0')
+//       modal.classList.toggle('pointer-events-none')
+//       body.classList.toggle('modal-active')
+//     }
 
     // swiper init
-    // const swiper = new Swiper();
 
     var swiper = new Swiper('.swiper-container', {
         spaceBetween: 30,
@@ -79,3 +74,16 @@ var openmodal = document.querySelectorAll('.modal-open')
           prevEl: '.swiper-button-prev',
         },
       });
+    
+
+    // modalClose
+
+    (function () {
+        const modalTwo = document.getElementById('modal2');
+        const close = document.getElementById('dismiss');
+    
+        close.addEventListener('click', function() {
+            modalTwo.classList.toggle('hidden');
+        })
+      }());
+    
