9ed45d900b3865602b84e6ddd4e8833220468099
Andrew Mason <andrewmasonmedia@gmail.com>
2020-11-17 22:49:22 -0500

    Added Sal.js animation library local copy js + css for better performance

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = function () {
-    AOS.init({
-        easing: 'ease-in-out-back',
-        offset: 0,
-    });
+    // AOS.init({
+    //     easing: 'ease-in-out-back',
+    //     offset: 0,
+    // });
 
     (function () {  
         var modalBtns = document.querySelectorAll('.modal-open');
@@ -78,4 +78,7 @@ window.onload = function () {
         slide1.classList.remove('bg-green-100');
         slide3.classList.toggle('bg-blue-100');
     })
-}
+    // sal animation
+    sal();
+
+}//end onload
